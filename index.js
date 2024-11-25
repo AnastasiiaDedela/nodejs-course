c88dc0f2de72ed4b7351a8309d8f5b848bb8276f
Anastasiia <nastyadedela@gmail.com>
2024-11-25 20:40:24 +0100

    feat: add functions for reading and writing files

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hi');
+import fs from 'fs';
+
+fs.readFile('test.txt', 'utf-8', (err, data) => {
+  fs.writeFile('test.txt', data + 'hello', (err, data) => {
+    console.log('it works');
+  });
+});
