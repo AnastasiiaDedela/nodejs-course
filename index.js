fb2b3cd64423a05334b293bd9f60bd1c00dc5ef2
Anastasiia <nastyadedela@gmail.com>
2024-11-27 21:33:56 +0100

    feat: add request hander

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import fs from 'fs';
 
 let server = http.createServer((request, response) => {
   response.writeHead(200, { 'content-type': 'text/html; charset=utf-8' });
-  const stream = fs.createReadStream('./templates/index.html');
-  stream.pipe(response);
+  if (request.url === '/') fs.createReadStream('./templates/index.html').pipe(response);
+  else if (request.url === '/about') fs.createReadStream('./templates/about.html').pipe(response);
+  else fs.createReadStream('./templates/error.html').pipe(response);
 });
 
 const PORT = 4598;
