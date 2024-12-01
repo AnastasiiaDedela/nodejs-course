f109dccfbb1f380c93a34541d974587df3f44d46
Anastasiia <nastyadedela@gmail.com>
2024-12-01 21:40:50 +0100

    feat: add request data validation

--- src/twit/twit.controller.js
+++ src/twit/twit.controller.js
@@ -5,6 +5,11 @@ const router = Router()
 const twitSerwice = new TwitService()
 
 router.post('/', (req, res)=>{
+    if(req.body?.text.length < 1){
+        return res.status(400).json({
+            message: "text is required"
+        })
+    }
     const twit = twitSerwice.createTwit(req.body)
     res.status(200).json(twit)
 })
