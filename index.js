1bc488426584da9ae684766f20b1d7c83beb4cdb
Anastasiia <nastyadedela@gmail.com>
2024-12-06 18:55:38 +0100

    feat: add path for errors

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import { twitRouter } from './src/twit.controller.js';
 const app = express();
 
 async function main() {
   app.use(express.json());
   app.use('/api/twits', twitRouter); 
+
+  app.all('*', (req, res)=>{
+    res.status(404).json({message: "not found"})
+  })
 
   app.listen(4200, () => {
     console.log('Server is running on port 4200');
